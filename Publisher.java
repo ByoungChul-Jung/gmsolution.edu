64b0f134ccbf367f3997ceed7c6e90edf224bbcf
이영주 <youngjoo.korea@gmail.com>
2017-04-20 21:45:25 +0900

    메서드 추가

--- Publisher.java
+++ Publisher.java
@@ -2,5 +2,6 @@ public interface Publisher {
     public void add(Observer observer);
     public void delete(Observer observer);
     public void notifyObserver();
+    public void toString();
 	//馬馬馬馬馬馬馬
 }
